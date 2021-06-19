43c73dbfe1937d6b14d8227bee1198f21e58eec3
Leonardo Mota <leo_lmota@hotmail.com>
2021-06-19 01:24:59 -0300

    REST testado

---
+++ src/main/java/com/leonardomota/cursomc/resources/CategoriaResource.java
@@ -0,0 +1,17 @@
+package com.leonardomota.cursomc.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResource {
+	
+	// Testando requisição REST
+	@RequestMapping(method=RequestMethod.GET)
+	public String listar() {
+		return "REST está funcionando!";
+	}
+	
+}
